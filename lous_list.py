8b1a1c7603f512e8a5bbf277ed54c05ae3b711cb
Brian Truong <bqtruong@cox.net>
2016-03-20 17:00:55 -0400

    lous_list.py optimize

--- lous_list.py
+++ lous_list.py
@@ -14,19 +14,17 @@ def class_search(dept_name, has_seats_available=True, level=None, not_before=Non
     courses = []
     for course in allcourses:
         course_info = course.decode("UTF-8").strip().split(";")
-        good_course = True
         if has_seats_available is True:
             if course_info[15] > course_info[16]:
-                good_course = False
+                continue
         if level != None:
             if course_info[1][0] != str(level)[0]:
-                good_course = False
+                continue
         if not_before != None:
             if int(course_info[12]) < not_before:
-                good_course = False
+                continue
         if not_after != None:
             if int(course_info[13]) > not_after:
-                good_course = False
-        if good_course == True:
-            courses.append(course_info)
+                continue
+        courses.append(course_info)
     return courses
